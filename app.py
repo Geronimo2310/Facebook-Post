422f76f90e66db2042d447e9d6989f4cc92cd5d6
Daniel Glasemann <danielglasemann@MacBook-Pro-von-Daniel.local>
2024-06-01 12:27:40 +0200

    Update app.py to use PORT environment variable

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +20,20 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Heroku!'
+
+@app.route('/welcome')
+def welcome():
+    return 'Welcome to your new Heroku app!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
